457fcf67faf3962c915258e72f1e6b985682ece4
CHETAN\Chetan <chetansidea@gmail.com>
2017-05-14 14:36:39 +0530

    added

--- src/Videos/Video.java
+++ src/Videos/Video.java
@@ -2,10 +2,5 @@ package Videos;
 
 public class Video {
     
-        System.out.print("abcd");
-    int a;
-    int b;
-    float cf;
-    float b;
-        System.out.print("argter");
+        System.out.print("abcdefgh");
 }
